a625ae191c9c132d580eb0c8a0e20564a1547d6f
Tobias Ammann <tobias-ammann@gmx.de>
2022-01-04 21:48:26 +0100

    Remove onlyoffice and elasticsearch docker unit files, get rid of docker due to log4j exploit

--- backup-seafile.sh
+++ backup-seafile.sh
@@ -156,8 +156,6 @@ start_user_service(){
   #===== Transfer systemd unit files ===============================================
 
   echo "Transfer systemd unit files"
-  rsync $RSYNC_OPT -e "ssh -o ControlPath=$SSH_CONTROL_PATH" $SSH_HOST:$SYSTEMD_ONLYOFFICE $DIR_OUTPUT
-  rsync $RSYNC_OPT -e "ssh -o ControlPath=$SSH_CONTROL_PATH" $SSH_HOST:$SYSTEMD_ELASTICSEARCH $DIR_OUTPUT
   rsync $RSYNC_OPT -e "ssh -o ControlPath=$SSH_CONTROL_PATH" $SSH_HOST:$SYSTEMD_MNT $DIR_OUTPUT
   rsync $RSYNC_OPT -e "ssh -o ControlPath=$SSH_CONTROL_PATH" $SSH_HOST:$SYSTEMD_USER_SEAFILE $DIR_OUTPUT
   rsync $RSYNC_OPT -e "ssh -o ControlPath=$SSH_CONTROL_PATH" $SSH_HOST:$SYSTEMD_USER_SEAHUB $DIR_OUTPUT
